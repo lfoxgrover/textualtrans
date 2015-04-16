7638b09ef96168dfd32223d3a8613a590e84c19a
lfoxgrover <laura.foxgrover@yale.edu>
2015-04-16 18:23:23 +0000

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
-var feeds = require('../feeds.js');
 
 module.exports = router;
-
-
 
 
 // =========================================================
@@ -38,77 +34,159 @@ mongoose.connect(process.env.MONGOLAB_URI || ('mongodb://' + process.env.IP + '/
 // =   DEFINE OUR DATA MODELS
 // =
 
-// Define the data structure of a Phrase model
-// It has width, height, top, and left attributes, which are required to be numbers from 0–100
-// And it has a color attribute, which is optionaland is a string (text)
+// Define the data structure of a Text Item models (tables)
 // Allowed data types (Number, String, Date...): http://mongoosejs.com/docs/schematypes.html
 
-var Phrase = mongoose.model('Phrase', {
-  preposition: {type: String, required: true},
-  noun: {type: String, required: true},
+
+var Textitem = mongoose.model('Textitem', {
+  entry: {type: String, required: true},
+  length:{type: Number, required: false},
+  longwords:{type: String, required: false},
 });
 
 
-
+var Textitem2 = mongoose.model('Textitem2', {
+  entry: {type: String, required: true},
+  length:{type: Number, required: false},
+  longwords:{type: String, required: false},
+});
 
 
 // =========================================================
-// =
-// =   WEB ROUTES
-// =
+// =   TEXT PROCESSING 
 
 
+var inputlengthcalc = function(input){
+  var inputlength = input.length;
+  return inputlength;
+  
+};
+
+
+// input text to words array
+var inputwords = function(input){
+  // using regex to get the individual words out of the inpur string: http://codereview.stackexchange.com/questions/65049/finding-longest-word-in-a-string
+  var textwords = input.replace(/[^A-Za-z\s]/g, "").split(" ");
+  return textwords;
+};
+
+
+var longwords = function(input){
+  var textwords = inputwords(input);
+  var wordsofcertainlength =[]; // in this case we're using five, we could do this in setup
+  
+  for (var i = 0; i < textwords.length; i++ ){
+    var texttomeasure = textwords[i];
+    if (texttomeasure.length > 6) {
+    wordsofcertainlength.push(texttomeasure);
+    }
+  }
+ 
+  var wordsofcertainlength = wordsofcertainlength.reverse().join(" ");
+  
+  return wordsofcertainlength.toString();
+  
+};
+
+  
+  
 // HOME PAGE
-// /
-// Shows _all_ the phrases
-
+//
 router.get('/', function(request, response, toss) {
+  
   
   // When the server receives a request for "/", this code runs
 
-  // Find all the Shape records in the database
-  Phrase.find(function(err, phrases) {
+  // Find all the records in the database
+  Textitem.find(function(err, textitems) {
     // This code will run once the database find is complete.
-    // phrases will contain a list (array) of all the phrases that were found.
-    // err will contain errors if any.
+    // textitems will contain a list (array) of all the textitems that were found.
 
     // If there's an error, tell Express to do its default behavior, which is show the error page.
     if (err) return toss(err);
     
-    // The list of shapes will be passed to the template.
+    // The list of textitems will be passed to the template.
     // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
-    response.locals.phrases = phrases;
-    
-    // Also pass the temperature, wind direction, and next bus arrival.
-    // This can crash if the data hasn't loaded for whatever reason, so toss to Express's error page in that case.
-    try {
+    response.locals.textitems = textitems;
       
-      // Use Moment.js to calculate the next bus arrival time minus the current time,
-      // and display it in english e.g. "in 18 minutes".
-      // http://momentjs.com/docs/#/displaying/from/ (returns difference as an english string e.g. "18 minutes")
-      // http://momentjs.com/docs/#/displaying/difference/ (returns difference in milliseconds)
-      var now = moment();
-      var next_arrival = moment(feeds.bus[0].arrival_at);
-      var arriving_in = next_arrival.from(now);
-      var margin = next_arrival.diff(now) / 1000;
-
-      response.locals.temperature = feeds.weather.main.temp;
-      response.locals.font_size = feeds.weather.main.temp / 2;
-      response.locals.wind_direction = feeds.weather.wind.deg;
-      response.locals.arriving_in = arriving_in;
-      response.locals.margin = margin;
-      response.locals.route = feeds.bus[0].route_id;
-      
-    }
-    catch(err) {
-      return toss(err);
-    }
+    // catch(err) {
+    //   return toss(err);
+    // }
     
-    // layout tells template to wrap itself in the "layout" template (located in the "views" folder).
     response.locals.layout = 'layout';
-
-    // Render the "home" template (located in the "views" folder).
+  
+  // Render the "home" template (located in the "views" folder).
     response.render('home');
+
+  });
+ 
+});
+
+
+
+// SOLO PAGE
+//
+router.get('/solo', function(request, response, toss) {
+  
+  // When the server receives a request for "/solo", this code runs
+  
+// Textitem2.collection.drop();
+
+  // Find all the records in the database
+  Textitem2.find(function(err, textitems) {
+    // This code will run once the database find is complete.
+    // textitems will contain a list (array) of all the textitems that were found.
+
+    // If there's an error, tell Express to do its default behavior, which is show the error page.
+    if (err) return toss(err);
+    
+    // The list of textitems will be passed to the template.
+    // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
+    response.locals.textitems = textitems;
+    
+    // catch(err) {
+    //   return toss(err);
+    // }
+    
+    response.locals.layout = 'layout';
+  
+  // Render the "home" template (located in the "views" folder).
+    response.render('solo');
+
+  });
+  
+});
+
+
+// GROUP PAGE
+//
+router.get('/group', function(request, response, toss) {
+  
+  
+  // Textitem.collection.drop();
+  
+  // When the server receives a request for "/group", this code runs
+
+  // Find all the records in the database
+  Textitem.find(function(err, textitems) {
+    // This code will run once the database find is complete.
+    // textitems will contain a list (array) of all the textitems that were found.
+
+    // If there's an error, tell Express to do its default behavior, which is show the error page.
+    if (err) return toss(err);
+    
+    // The list of textitems will be passed to the template.
+    // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
+    response.locals.textitems = textitems;
+    
+    // catch(err) {
+    //   return toss(err);
+    // }
+    
+    response.locals.layout = 'layout';
+  
+  // Render the "group" template (located in the "views" folder).
+    response.render('group');
 
   });
   
@@ -117,23 +195,112 @@ router.get('/', function(request, response, toss) {
 
 
 
+
+// CREATE 1 PAGE - GROUP
+
+router.get('/create1', function(request, response, toss) {
+  
+  // When the server receives a request for "/create1", this code runs
+  
+  response.locals.layout = 'layout';
+
+  // Make a new Textitem in memory, with the parameters that come from the URL 
+  // ?width=25&height=25&top=25&left=25&color=#ff0000
+  // and store it in the entry variable
+  
+  var textitem = new Textitem({
+    entry: request.query.entry,
+    length: inputlengthcalc(request.query.entry),
+    longwords: longwords(request.query.entry),
+    author: request.query.author,
+    
+  });
+  
+  // Now save it to the database
+  textitem.save(function(err) {
+    // This code runs once the database save is complete
+
+    // An err here can be due to validations
+    if (err) return toss(err);
+
+  });
+ 
+  
+   // Otherwise render a "thank you" page
+    response.locals.textitem = textitem;
+    
+    response.redirect('/group');
+    // to send the user straight to the homepage after saving
+  
+});
+
+
+
+
+// CREATE 2 PAGE - SOLO
+
+router.get('/create2', function(request, response, toss) {
+  
+  // When the server receives a request for "/create2", this code runs
+  
+  response.locals.layout = 'layout';
+
+  // Make a new Textitem in memory, with the parameters that come from the URL 
+  // ?width=25&height=25&top=25&left=25&color=#ff0000
+  // and store it in the entry variable
+  
+  var textitem = new Textitem2({
+    entry: request.query.entry,
+    length: inputlengthcalc(request.query.entry),
+    longwords: longwords(request.query.entry),
+    author: request.query.author,
+    
+  });
+  
+  // Now save it to the database
+  textitem.save(function(err) {
+    // This code runs once the database save is complete
+
+    // An err here can be due to validations
+    if (err) return toss(err);
+
+  });
+ 
+  
+   // Otherwise render a "thank you" page
+    response.locals.textitem = textitem;
+    
+    response.redirect('/solo');
+    // to send the user straight to the homepage after saving
+  
+});
+
+
+
 // SHOW PAGE
 // /show?id=54e2058e85b156d10b064ca0
-// Shows a _single_ phrase
+// Shows a _single_ item
 
 router.get('/show', function(request, response, toss) {
   
   // When the server receives a request for "/show", this code runs
   
-  // Find a Phrase with this id
-  Phrase.findOne({_id: request.query.id}, function(err, phrase) {
+  // Find a Textitem with this id
+  Textitem.findOne({_id: request.query.id}, function(err, textitem) {
     // This code will run once the database find is complete.
     // phrase will contain the found phrase.
     // err will contain errors if any (for example, no such record).
 
     if (err) return toss(err);
     
-    response.locals.phrase = phrase;
+    var inputlength = inputlengthcalc(textitem.entry);
+    console.log(inputlength);
+    
+    var longwordsinentry = textitem.longwords;
+    
+    response.locals.textitem = textitem;
+    response.locals.inputlength = inputlength;
+    response.locals.longwordsinentry = longwordsinentry;
     response.locals.layout = 'layout';
     response.render('show');
     
@@ -149,7 +316,6 @@ router.get('/show', function(request, response, toss) {
 router.get('/new', function(request, response) {
 
   // When the server receives a request for "/new", this code runs
-  
   // Just render a basic HTML page with a form. We don't need to pass any variables.
 
   response.locals.layout = 'layout';
@@ -161,43 +327,35 @@ router.get('/new', function(request, response) {
 
 
 
-// CREATE PAGE
-// /create?width=25&height=25&top=25&left=25&color=#ff0000
-// Normally you get to this page by clicking "Submit" on the /new page, but
-// you could also enter a URL like the above directly into your browser.
 
-router.get('/create', function(request, response, toss) {
-  
-  // When the server receives a request for "/create", this code runs
-  
-  response.locals.layout = 'layout';
 
-  // Make a new Phrase in memory, with the parameters that come from the URL 
-  // ?width=25&height=25&top=25&left=25&color=#ff0000
-  // and store it in the shape variable
-  var phrase = new Phrase({
-    preposition: request.query.preposition,
-    noun: request.query.noun,
-  });
-  
-  // Now save it to the database
-  phrase.save(function(err) {
-    // This code runs once the database save is complete
 
-    // An err here can be due to validations
-    if (err) return toss(err);
+// router.get('/delete', function(request, response, toss) {
+  
+//   // When the server receives a request for "/delete", this code runs
+  
+//   response.locals.layout = 'layout';
+  
+//   var deleteid = request.query.id; 
+  
+//   console.log(deleteid);
+  
+//   var deleteitem = Textitem.find({ id:deleteid });
+
+  
+//   // Now delete it to the database
+//   Textitem.find({ _id:deleteid }).remove(function(err) {
+//     // This code runs once the database save is complete
+
+//     // An err here can be due to validations
+//     if (err) return toss(err);
     
-    // Otherwise render a "thank you" page
-    response.locals.phrase = phrase;
-    response.render('create');
-    
-    // Alternatively we could just do
-    // response.redirect('/');
-    // to send the user straight to the homepage after saving the new shape
+//     // Alternatively we could just do
+//     response.redirect('/');
+//     // to send the user straight to the homepage after saving the new shape
 
-  });
-  
-});
+//   });
+// });
 
 
 
